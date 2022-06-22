069a5c890c6a52105debf0f76bdd0cab4b506488
Frazer-dl <53271082+Frazer-dl@users.noreply.github.com>
2022-06-22 09:52:30 +0300

    Add files via upload

---
+++ js/scrypt.js
@@ -0,0 +1,10 @@
+function visibility_structure_1() {
+    document.getElementById("img_id").src = "img/structure.PNG"
+    document.getElementById("structure_descr_2").style.visibility = "hidden"
+    document.getElementById("structure_descr_1").style.visibility = "visible"
+}
+function visibility_structure_2() {
+    document.getElementById("img_id").src = "img/database_structure.PNG"
+    document.getElementById("structure_descr_1").style.visibility = "hidden"
+    document.getElementById("structure_descr_2").style.visibility = "visible"
+}
