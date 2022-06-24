85b4c4ff1ea992696ca768a0dba1c5eacdc6b2d5
Frazer-dl <53271082+Frazer-dl@users.noreply.github.com>
2022-06-24 15:54:59 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    $(".carousel__inner").slick({
+        speed: 500,
+        slidesToShow: 1,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_next.png"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                  arrows: false
+                }
+              }
+        ]
+      });
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.header'),
+  menuItem = document.querySelectorAll('.header__item'),
+  hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('header_active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('header_active');
+      })
+  })
+})
+
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.ideas-item__content').eq(i).toggleClass('ideas-item__content_active');
+            $('.ideas-item__descr').eq(i).toggleClass('ideas-item__descr_active');
+        })
+    });
+  };
+
+  toggleSlide('.ideas-item__link');
+  toggleSlide('.ideas-item__back');
